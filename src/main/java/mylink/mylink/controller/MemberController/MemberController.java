57a5953a2f202aee6361e7e16ea0b58025f3b605
JoongWonChoi <qweasd1130@naver.com>
2022-02-09 21:50:49 +0900

    update

--- src/main/java/mylink/mylink/controller/MemberController/MemberController.java
+++ src/main/java/mylink/mylink/controller/MemberController/MemberController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -141,5 +142,4 @@ public class MemberController {
         * */
 
     }
-
 }
