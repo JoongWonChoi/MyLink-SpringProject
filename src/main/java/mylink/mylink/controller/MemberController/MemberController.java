99ced1a46bb37d32545d8ef526ad385682234384
JoongWonChoi <qweasd1130@naver.com>
2022-02-10 17:27:27 +0900

    fix view template & remake

--- src/main/java/mylink/mylink/controller/MemberController/MemberController.java
+++ src/main/java/mylink/mylink/controller/MemberController/MemberController.java
@@ -77,10 +77,10 @@ public class MemberController {
     @PostMapping("members/{id}/edit")
     public String updateMember(@PathVariable("id") Long id, @ModelAttribute("form") MemberForm form) {
         /*MemberForm 은 웹 계층, controller에서의 사용 용도 클래스이기때문에, 서비스 로직으로 보내는 것은 좋지 않다고 생각
-        * 그렇다고 새로운 Member 엔티티 객체를 생성하는 것은, 엔티티를 마구잡이로 외부에서 반환하고 사용하는 것은 엔티티 고유의 역할을 방해할 수
-        * 있기 때문에 사용하지 않음. 엔티티 클래스는 순수하게 엔티티(DB의 테이블) 생성 기능에 충실해야한다고 생각함.
-        * 그러면 이런식으로 MemberForm으로 넘어온 폼의 데이터들을, 직접 꺼내어 서비스 단으로 보내는 방식을 사용하는데 이는 딱 봐도 좀 지저분함
-        * 어떻게 하면 좋을까...*/
+         * 그렇다고 새로운 Member 엔티티 객체를 생성하는 것은, 엔티티를 마구잡이로 외부에서 반환하고 사용하는 것은 엔티티 고유의 역할을 방해할 수
+         * 있기 때문에 사용하지 않음. 엔티티 클래스는 순수하게 엔티티(DB의 테이블) 생성 기능에 충실해야한다고 생각함.
+         * 그러면 이런식으로 MemberForm으로 넘어온 폼의 데이터들을, 직접 꺼내어 서비스 단으로 보내는 방식을 사용하는데 이는 딱 봐도 좀 지저분함
+         * 어떻게 하면 좋을까...*/
         memberService.updateMember(
                 id,
                 form.getName(),
@@ -111,17 +111,17 @@ public class MemberController {
     }*/
     /*===로그인===*/
     /*1. 내가 생각한 로직
-    * View의 로그인 폼에서는 보통 아이디(address)와 비밀번호(password)가 넘어온다. 다른 정보 기입은 본 적이 없음.
-    * 따라서 로그인 폼으로 넘어오는 아이디와 비밀번호 정보만을 갖고 로그인 검증을 해야한다고 생각.
-    * ::로그인 폼에서 address, password 바인딩 -> 컨트롤러를 통해 서비스 비즈니스 로직으로 전달
-    * ->저장소에서 address로 객체를 찾고(findByAddress), 해당 객체의 password가 파라미터로 넘어온 password와 일치하는지 확인
-    * -> 아이디와 비밀번호를 통해 기존의 객체 정보와 일치 불일치 여부를 다시 반환하는 트랜잭션 개발
-    * 최종 전달을 받는 컨트롤러에서는 객체가 반환되거나(일치) 그렇지 않다면 아무것도 반환되지 않을 것.(null)
-    *
-    * 개발 하며 알게된 점 : String은 엄연히 클래스이고 ==연산자와 equal()연산자의 차이를 잘 이해했어야 함 ㅎㅎ..*/
+     * View의 로그인 폼에서는 보통 아이디(address)와 비밀번호(password)가 넘어온다. 다른 정보 기입은 본 적이 없음.
+     * 따라서 로그인 폼으로 넘어오는 아이디와 비밀번호 정보만을 갖고 로그인 검증을 해야한다고 생각.
+     * ::로그인 폼에서 address, password 바인딩 -> 컨트롤러를 통해 서비스 비즈니스 로직으로 전달
+     * ->저장소에서 address로 객체를 찾고(findByAddress), 해당 객체의 password가 파라미터로 넘어온 password와 일치하는지 확인
+     * -> 아이디와 비밀번호를 통해 기존의 객체 정보와 일치 불일치 여부를 다시 반환하는 트랜잭션 개발
+     * 최종 전달을 받는 컨트롤러에서는 객체가 반환되거나(일치) 그렇지 않다면 아무것도 반환되지 않을 것.(null)
+     *
+     * 개발 하며 알게된 점 : String은 엄연히 클래스이고 ==연산자와 equal()연산자의 차이를 잘 이해했어야 함 ㅎㅎ..*/
 
     @GetMapping("/login")
-    public String loginForm(@ModelAttribute("memberForm")MemberForm memberForm) {
+    public String loginForm(@ModelAttribute("memberForm") MemberForm memberForm) {
         return "MemberService/login";
     }
 
@@ -139,15 +139,14 @@ public class MemberController {
             session.setAttribute("pw", memberForm.getPassword());
             System.out.println("로그인 완료");
         }*/
-        HttpSession session = (HttpSession) request.getSession(true);
+        HttpSession session = request.getSession(true);
         Member loginMember = memberService.login(memberForm.getAddress(), memberForm.getPassword());
 
         if (loginMember != null) {
-            session.setAttribute("loginMember",loginMember);
-            System.out.println("sfasf"+session.getAttribute("loginMember").toString());
-            System.out.println(session.getAttribute("loginMember"));
-            return "redirect:/";}
-        else {
+            System.out.println(loginMember.getId());
+            session.setAttribute("loginMember", loginMember);
+            return "redirect:/";
+        } else {
             FieldError fieldError = new FieldError("memberForm", "address", "아이디가 일치하지 않거나 존재하지 않는 아이디 입니다.");
             bindingResult.addError(fieldError);
             return "MemberService/login";
@@ -156,6 +155,16 @@ public class MemberController {
          * 해당하는 멤버가 있어도 변화하는 것은 없음. 즉 로그인 성공하면 내 정보를 보는 등의 변화가 있었으면 좋겠음.
          * */
 
+    }
+    // 로그아웃 ==> HTTP 세션에 저장되어있던 로그인 된 멤버 객체 삭제
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        System.out.println("for logout   //  "+session.getAttribute("loginMember"));
+        if(session!=null){
+            session.removeAttribute("loginMember");
+        }
+        return "redirect:/";
     }
 }
 
